1803f567d6e19ce3cf4a723b6724390eb0516320
SleepingFores7s - Laptop <Daniel.Evensson@hotmail.com>
2026-04-09 01:25:37 +0200

    Fix path in base_config.js for correct GitHub Pages deployment

--- scripts/base_config.js
+++ scripts/base_config.js
@@ -1,6 +1,6 @@
 const root = window.location.origin;
 let path = "/";
 if (window.location.hostname.includes("github.io")) {
-  path = "/Frontend-Projektarbete-VG-niva/";
+  path = "/Frontend-Project-VG/";
 }
 document.getElementById("base-path").setAttribute("href", root + path);
